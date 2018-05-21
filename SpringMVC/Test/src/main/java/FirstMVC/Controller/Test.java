0a8a4d76e1b1ceabff57e0eed1ed46c0eb897106
limeng <ldx19980108@gmail.com>
2018-05-21 21:15:32 +0800

    Spring MVC demo样板

---
+++ SpringMVC/Test/src/main/java/FirstMVC/Controller/Test.java
@@ -0,0 +1,15 @@
+package FirstMVC.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/main")
+public class Test {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String welcome(){
+        return "home";
+    }
+}
